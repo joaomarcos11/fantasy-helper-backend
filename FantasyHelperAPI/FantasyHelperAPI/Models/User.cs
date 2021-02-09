06695c2a8c74489c01250afb568b73f3ffd888b7
Joao Marcos <joaomarcos11.tech@gmail.com>
2021-02-09 15:16:28 -0300

    Criando Models

---
+++ FantasyHelperAPI/FantasyHelperAPI/Models/User.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace FantasyHelperAPI.Models
+{
+    public class User
+    {
+        public short IdUser { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string UserPassword { get; set; }
+        public string Avatar { get; set; } = null;
+        public bool IsActive { get; set; } = true;
+        public bool IsAdmin { get; set; } = false;
+        public List<WeekStats> WeekStatsList { get; set; }
+    }
+}
---
+++ FantasyHelperAPI/FantasyHelperAPI/Models/WeekStats.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace FantasyHelperAPI.Models
+{
+    public class WeekStats
+    {
+        public short IdStats { get; set; }
+        public byte WeekNumber { get; set; }
+        public byte Games { get; set; }
+        public short Fga { get; set; }
+        public short Fgm { get; set; }
+        public short Fta { get; set; }
+        public short Ftm { get; set; }
+        public byte ThreePoints { get; set; }
+        public short Points { get; set; }
+        public short Rebounds { get; set; }
+        public short Assists { get; set; }
+        public byte Steals { get; set; }
+        public byte Blocks { get; set; }
+        public byte Turnovers { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+        public short IdUser { get; set; }
+        public User User { get; set; }
+    }
+}
